216ca9a639b0f745e1181fdd8e366052bd4240a8
Umar-Rehman <u.rehman_97@hotmail.co.uk>
2021-08-04 15:46:50 +0100

    retrieve text content of url

---
+++ pythonTask.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://en.wikipedia.org/wiki/Quantum_mechanics'
+res = requests.get(url)
+html_page = res.content
+soup = BeautifulSoup(html_page, 'html.parser')
+html_text = soup.find_all(text=True)
+
+# print(output)
+f = open("html_text.txt", "w", encoding="utf-8")         # Creating html_text.txt File
+
+for line in html_text:
+	f.write(line)
+
+f.close()
